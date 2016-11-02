e4e7a5ca75ff77e86441b15491ee3d0dbb314910
RomaVan <lasedwes@rambler.ru>
2016-11-02 23:52:22 +0200

    MySQL

--- config.php
+++ config.php
@@ -1,12 +1,5 @@
 <?php
 
-if ( !$_SESSION['cart'] ) {
-    $_SESSION['cart'] = [];
-}
-
-define( "MESSAGE_PATH", 'database/info.txt');
-
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-$config_ini = parse_ini_file('Config.ini');
--- templates/catalog.view.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<div class='container'>";
-foreach ($data as $value){
-    $url = '/catalog/'.$value['id'];
-    echo  "<a href='$url' >".$value['title']."</a><br>";
-
-}
-echo "</div>";
